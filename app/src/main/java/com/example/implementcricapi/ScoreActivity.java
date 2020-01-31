59fba71b78aaf8afdc195d3039fc234203d25a75
Basit-Technolligence <pakkobra007@gmail.com>
2020-01-31 17:46:31 +0500

    done live score data

--- app/src/main/java/com/example/implementcricapi/ScoreActivity.java
+++ app/src/main/java/com/example/implementcricapi/ScoreActivity.java
@@ -135,7 +135,7 @@ public class ScoreActivity extends AppCompatActivity {
                                         dref.child("LiveScore").child("Peshawar Zalmi").child("rr").setValue(String.valueOf(s / o));
 
                                 }
-                                if (teamid.equals("51")) {
+                                if (teamid.equals("53")) {
                                     team.setText("Quetta Gladiators");
                                     dref.child("LiveScore").child("Quetta Gladiators").child("score").setValue(scoret);
                                     dref.child("LiveScore").child("Quetta Gladiators").child("wicket").setValue(wickett);
@@ -158,7 +158,7 @@ public class ScoreActivity extends AppCompatActivity {
                                     rr2.setText(String.valueOf(s / o));
                                 else
                                     rr2.setText("0");
-                                if (teamid.equals("53")) {
+                                if (teamid.equals("51")) {
                                     team2.setText("Islamabad United");
                                     dref.child("LiveScore").child("Islamabad United").child("score").setValue(scoret);
                                     dref.child("LiveScore").child("Islamabad United").child("wicket").setValue(wickett);
@@ -249,6 +249,7 @@ public class ScoreActivity extends AppCompatActivity {
         });
         RequestQueue requestQueue = Volley.newRequestQueue(this);
         requestQueue.add(stringRequest);
+        loadData();
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -256,5 +257,140 @@ public class ScoreActivity extends AppCompatActivity {
             }
         }, 5000);
 
+
     }
+
+    private void loadData(){
+        String dataUrl="https://cricket.sportmonks.com/api/v2.0/fixtures/14985?api_token=9ivr14EK7LFO5sg5SSQLg7Fby8NHWL8vJ8MkzsHgm0Y5WvFblbMubiYRlVZf&include=scoreboards,localTeam,visitorTeam,batting,bowling,balls,lineup";
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, dataUrl, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject responseObject = new JSONObject(response);
+                    JSONObject dataObject = responseObject.getJSONObject("data");
+                    //JSONObject ballsObject = dataObject.getJSONObject("ball");
+                    JSONArray ballsObject = new JSONObject(String.valueOf(dataObject)).getJSONArray("balls");
+
+                    String batsman1=ballsObject.getJSONObject(ballsObject.length()-1).getString("batsman_one_on_creeze_id");
+                    String batsman2=ballsObject.getJSONObject(ballsObject.length()-1).getString("batsman_two_on_creeze_id");
+                    //JSONObject battingObject=dataObject.getJSONObject("batting");
+                    JSONArray playerArray=new JSONObject(String.valueOf(dataObject)).getJSONArray("batting");
+                    for(int j=0;j<playerArray.length();j++){
+                        String playerId=playerArray.getJSONObject(j).getString("player_id");
+                        if(playerId.equals(batsman1)){
+                            String score=playerArray.getJSONObject(j).getString("score");
+                            String balls=playerArray.getJSONObject(j).getString("ball");
+                            String fours=playerArray.getJSONObject(j).getString("four_x");
+                            String sixs=playerArray.getJSONObject(j).getString("six_x");
+                            String rr=playerArray.getJSONObject(j).getString("rate");
+                            dref.child("LiveScore").child("batsmanScore").child("1").child("score").setValue(score);
+                            dref.child("LiveScore").child("batsmanScore").child("1").child("balls").setValue(balls);
+                            dref.child("LiveScore").child("batsmanScore").child("1").child("fours").setValue(fours);
+                            dref.child("LiveScore").child("batsmanScore").child("1").child("sixs").setValue(sixs);
+                            dref.child("LiveScore").child("batsmanScore").child("1").child("rr").setValue(rr);
+                            getPlayerData(batsman1,1);
+                        }
+                        if(playerId.equals(batsman2)){
+                            String score=playerArray.getJSONObject(j).getString("score");
+                            String balls=playerArray.getJSONObject(j).getString("ball");
+                            String fours=playerArray.getJSONObject(j).getString("four_x");
+                            String sixs=playerArray.getJSONObject(j).getString("six_x");
+                            String rr=playerArray.getJSONObject(j).getString("rate");
+                            dref.child("LiveScore").child("batsmanScore").child("2").child("score").setValue(score);
+                            dref.child("LiveScore").child("batsmanScore").child("2").child("balls").setValue(balls);
+                            dref.child("LiveScore").child("batsmanScore").child("2").child("fours").setValue(fours);
+                            dref.child("LiveScore").child("batsmanScore").child("2").child("sixs").setValue(sixs);
+                            dref.child("LiveScore").child("batsmanScore").child("2").child("rr").setValue(rr);
+                            getPlayerData(batsman2,2);
+                        }
+                    }
+                    JSONObject bowlerObject= ballsObject.getJSONObject(ballsObject.length()-1);
+                    JSONObject nameObject=bowlerObject.getJSONObject("bowler");
+                    String bowlerName = nameObject.getString("fullname");
+                    dref.child("LiveScore").child("bowler").setValue(bowlerName);
+                    JSONObject batsmanObject=bowlerObject.getJSONObject("batsman");
+                    String batsmanName = batsmanObject.getString("fullname");
+                    dref.child("LiveScore").child("currentBatting").setValue(batsmanName);
+                    int count=6;
+                    for(int i=ballsObject.length()-1;i>=ballsObject.length()-6;i--){
+                        try {
+                            JSONObject recentObject= ballsObject.getJSONObject(i);
+                            JSONObject scoreObject= recentObject.getJSONObject("score");
+                            String name=scoreObject.getString("name");
+                            if(name.contains("Out") || name.contains("Bowled")|| name.contains("OUT")){
+                                name="W ";
+                            }
+                            else if(name.contains("No")){
+                                name="0 ";
+                            }
+                            else if(name.contains("Runs")){
+                                name=name.replace("Runs","");
+                            }
+                            else if(name.contains("Run")){
+                                name=name.replace("Run","");
+                            }
+                            else if(name.contains("Wide")){
+                                name=name.replace(" Wide","wd ");
+                            }
+                            else if(name.contains("FOUR")){
+                                name="4 ";
+                            }
+                            else if(name.contains("SIX")){
+                                name="6 ";
+                            }
+
+                            dref.child("LiveScore").child("recent").child(String.valueOf(count)).setValue(name);
+                            count--;
+
+                        } catch (Exception e) {
+                            Toast.makeText(getApplicationContext(), "Error " + e.getMessage(), Toast.LENGTH_LONG).show();
+                        }
+
+                    }
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Toast.makeText(getApplicationContext(), "Error " + e.getMessage(), Toast.LENGTH_LONG).show();
+
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
+    private  void getPlayerData(String id, final int x){
+        String playerUrl="https://cricket.sportmonks.com/api/v2.0/players/"+id+"?api_token=9ivr14EK7LFO5sg5SSQLg7Fby8NHWL8vJ8MkzsHgm0Y5WvFblbMubiYRlVZf";
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, playerUrl, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                JSONObject responseObject = null;
+                try {
+                    responseObject = new JSONObject(response);
+                    JSONObject dataObject = responseObject.getJSONObject("data");
+                    String playerName=dataObject.getString("fullname");
+                    dref.child("LiveScore").child("batsmanScore").child(String.valueOf(x)).child("name").setValue(playerName);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Toast.makeText(getApplicationContext(), "Error " + e.getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+
+    }
+
 }
